10563b54c73a066ddb8dab32bf0e408302554fff
kang-heewon <itphantom@icloud.com>
2019-01-09 02:09:59 +0900

    change port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const app = express()
-const port = 80
+const port = 8080
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
